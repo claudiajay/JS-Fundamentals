161860f32e810ac03a8b68f7914130e3ebd3e432
claudiajay <claudiajoppa@gmail.com>
2025-07-17 09:11:25 +0000

    loop to languages

---
+++ 6-multi_languages_loop.js
@@ -0,0 +1,9 @@
+let l1 = "C is fun";
+let l2 = "Python is cool";
+let l3 = "JavaScript is amazing";
+
+const sentences = [l1, l2, l3];
+
+for (let i = 0; i < sentences.length; i++) {
+  console.log(sentences[i]);
+}
