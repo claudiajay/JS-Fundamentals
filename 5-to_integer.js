799844345b907dbd037a3237ca97b08c323e58e7
claudiajay <claudiajoppa@gmail.com>
2025-07-17 09:07:44 +0000

    an integer

--- 5-to_integer.js
+++ 5-to_integer.js
@@ -2,7 +2,7 @@ const arg = process.argv[2];
 const num = parseInt(arg);
 
 if(!isNaN(num)){
-  console.log(`My number is: ${num}`);
+  console.log(`My number: ${num}`);
 } else {
   console.log('Not a number');
 }
